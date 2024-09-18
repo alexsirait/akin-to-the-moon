982af73b77b64a4d73e77b68d923b7a4e9353184
ALEX SIRAIT <alexsirait1001@gmail.com>
2024-09-18 19:09:41 +0700

    punya akin

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function listuser()
+    {
+        // satuan
+        $datauser = DB::table('tbl_user')->get();
+
+        return view('user', [
+            "user" => $datauser,
+        ]);
+    }
+
+    public function adduser(Request $r)
+    {
+        return view('adduser');
+    }
+
+    public function simpan_user(Request $r)
+    {
+        $masuk_name = $r->name;
+        $hp = $r->hp;
+        $id = $r->id;
+
+        DB::table('tbl_user')->insert([
+            "name" => $masuk_name,
+            "hp" => $hp,
+            "id" => $id,
+        ]);
+
+        return redirect('/user');
+    }
+
+    // delete
+    public function delete_user(Request $r)
+    {
+        $id = $r->id;
+
+        DB::table('tbl_user')->where("id", $id)->delete();
+
+        return redirect('/user');
+    }
+
+
+
+
+
+    // edit
+    public function edit_user(Request $r)
+    {
+        $id = $r->id;
+
+        $rasd = DB::table('tbl_user')->where("id", $id)->first();
+
+        $data = [
+            "data" => $rasd
+        ];
+
+        return view('edit_user', $data);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function update_user(Request $r)
+    {
+        $id = $r->id;
+        $name = $r->name;
+
+        DB::table('tbl_user')->where("id", $id)->update([
+            "name" => $name,
+        ]);
+
+        return redirect('/user');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrancoController;
 use App\Http\Controllers\FrankController;
 use App\Http\Controllers\OrangController;
+use App\Http\Controllers\UserController;
 
 Route::get('/listhpmahal', [FrancoController::class, 'listhpmahal']);
 Route::get('/hargahpmahal', [FrancoController::class, 'hargahpmahal_func']);
@@ -18,5 +19,51 @@ Route::get('/simpan_hewan', [FrankController::class, 'simpan_hewan']);
 
 Route::get('/orang', [OrangController::class, 'listorang']);
 Route::get('/addorang', [OrangController::class, 'addorang']);
-
 Route::get('/simpan_orang', [OrangController::class, 'simpan_orang']);
+
+Route::get('/user', [UserController::class, 'listuser']);
+Route::get('/adduser', [UserController::class, 'adduser']);
+Route::get('/simpan_user', [UserController::class, 'simpan_user']);
+
+Route::get('/deleteuser', [UserController::class, 'delete_user']);
+
+
+
+
+// edit
+Route::get('/edit_user', [UserController::class, 'edit_user']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/update_user', [UserController::class, 'update_user']);
+
+
+
+
